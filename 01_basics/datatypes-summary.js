e1cd4e460d2a7c49d831c22e7120196d27321466
_vipinpal <68026206+Vipin4597@users.noreply.github.com>
2024-08-15 20:30:08 +0530

    Update datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,6 +1,10 @@
-//  Primitive
+//  Primitive (value type)
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+//type of Null: object
+//type of undefines: undefines
+//type of Number: number
+//type of String: string
 
 const score = 100
 const scoreValue = 100.3
@@ -14,7 +18,7 @@ const anotherId = Symbol('123')
 
 console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
+// const bigNumber = 3456543576654356754n  // n add karne se automatically bitInt me convert kar deta hai
 
 
 
@@ -33,5 +37,15 @@ const myFunction = function(){
 }
 
 console.log(typeof anotherId);
+
+let a=["1", "2"]
+let obj={
+    name: "vipin"
+}
+let func= function(){
+}
+console.log(typeof a); // object
+console.log(typeof obj); //object
+console.log(typeof func); //function
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
