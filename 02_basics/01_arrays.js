fa62268b3192f3d6bda9e4294d2775f4861ee90c
_vipinpal <68026206+Vipin4597@users.noreply.github.com>
2024-08-18 17:09:16 +0530

    Update 01_arrays.js

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,6 +1,6 @@
 // array
 
-const myArr = [0, 1, 2, 3, 4, 5]
+const myArr = [0, 1, 2, 3, 4, 5]  // can be of different types, resizable, copying an array creates a shallow copy(refernce type)
 const myHeors = ["shaktiman", "naagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
@@ -12,8 +12,8 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.push(7)
 // myArr.pop()
 
-// myArr.unshift(9)
-// myArr.shift()
+// myArr.unshift(9) // adds the 9 at the 0th position by shifting the entire array one positin ahead
+// myArr.shift() //remove the first element of the array and shift the array one postion behind
 
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
@@ -28,12 +28,12 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(1, 3) // returns the section of array , from start to end(end not inclusive), slice does not modify the original array
 
 console.log(myn1);
 console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(1, 3) // splice is same as slice its just that ye original array se utna portion remove kar dta hai
 console.log("C ", myArr);
 console.log(myn2);
