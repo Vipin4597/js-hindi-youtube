9427c2aa5aca33aef6a3237a2832007a03ec2edf
_vipinpal <68026206+Vipin4597@users.noreply.github.com>
2024-08-21 12:40:53 +0530

    Update four.js

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,16 +1,19 @@
+//for in: can be used only with iterables
+
 const myObject = {
     js: 'javascript',
     cpp: 'C++',
     rb: "ruby",
     swift: "swift by apple"
 }
-
+//for of loops doesnt work with objects
+//we use for in loop for objects
 for (const key in myObject) {
     //console.log(`${key} shortcut is for ${myObject[key]}`);
 }
 
 const programming = ["js", "rb", "py", "java", "cpp"]
-
+ // for in when used with arrays, gives the index
 for (const key in programming) {
     //console.log(programming[key]);
 }
@@ -22,5 +25,5 @@ for (const key in programming) {
 // map.set('IN', "India")
 
 // for (const key in map) {
-//     console.log(key);
+//     console.log(key);  // will not print anything as map is not iterabel for in will not work
 // }
